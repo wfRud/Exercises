626dd40df8b9dbdac39577ad1dd652e58af72223
Filip Rudek <rudek.filip@gmail.com>
2019-12-18 13:53:44 +0100

    Square each number passed into function and then sums the results

---
+++ Zadanie 5/index.js
@@ -0,0 +1,23 @@
+// ===Complete the square sum function so that it squares each number passed into it and then sums the results together.===
+// ===For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.===
+
+// function squareSum(numbers) {
+//   var total = 0;
+
+//   for (var i = 0; i < numbers.length; i++) {
+//     total += numbers[i] * numbers[i];
+//   }
+
+//   return total;
+// }
+
+function squareSum(numbers) {
+  return numbers.reduce((sum, n) => {
+    return sum + n * n;
+  }, 0);
+}
+
+const arr = [1, 2];
+const arr1 = [-11, 17, 17, -10, 7, 17, -13, -9, -6];
+
+console.log(squareSum(arr1));
